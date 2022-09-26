b6f67ac04bcac20462ede05dc674726fddcbf0db
EnixNZXT <91321320+EnixNZXT@users.noreply.github.com>
2022-09-26 23:16:18 +0200

    static tabs implemented

--- src/F1/GUI.java
+++ src/F1/GUI.java
@@ -2,65 +2,64 @@ package F1;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class GUI extends JFrame {
-
-public GUI(){
+    JFrame window;
+    JTabbedPane tabbedOuterPane;
+    JTabbedPane tabbedInnerPane;
+    JLabel tabTitelLabel;
+    JLabel overviewLabel,driverLabel,teamLabel,trackLabel;
+    JTextArea overviewTextArea,teamTextArea, trackTextArea;
+    int count;
+public GUI() {
     //window
-    JFrame window =new JFrame();
+    window = new JFrame();
     window.setTitle("F1 Crossplay Season Manager");
-    window.setSize(1024,768);
+    window.setSize(1024, 768);
     window.setDefaultCloseOperation(EXIT_ON_CLOSE);
     window.setLocationRelativeTo(null);
     window.setResizable(true);
 
     //tab pane
-    JTabbedPane tabbedOuterPane =new JTabbedPane();
+    tabbedOuterPane = new JTabbedPane();
+    count=2022;
+    do {
 
-
-
-// first tab
-
-    JPanel firstPanel=new JPanel();
-    GridLayout gridLayout=new GridLayout();
-    firstPanel.setLayout(gridLayout);
-    //stuff in tab
-    JTabbedPane tabbedinnerPane=new JTabbedPane(JTabbedPane.LEFT);
-    JLabel overviewLabel =new JLabel("Overview");
-    overviewLabel.setPreferredSize(new Dimension(75,25));
-    JTextArea overviewTextArea=new JTextArea();
-
-
-
-    JPanel secondPanel=new JPanel();
-    GridLayout gridLayout2=new GridLayout();
-    secondPanel.setLayout(gridLayout2);
-
-    JPanel thirdpanel=new JPanel();
-    GridLayout gridLayout3=new GridLayout();
-    thirdpanel.setLayout(gridLayout3);
-
-    //add tab on tabpane
-
-    tabbedOuterPane.add("2022",firstPanel);
-    tabbedOuterPane.add("2023",secondPanel);
-    tabbedOuterPane.add("2024",thirdpanel);
-
-
-
-    //add tabpane on window
-    window.add(tabbedOuterPane);
+        InnerTab(count);
+        count++;
+    }while (count<=2030);
     window.setVisible(true);
-
-
-
 }
 
-    public static void main(String[] args) {
+public void InnerTab(int count){
+
+
+    tabbedInnerPane = new JTabbedPane(JTabbedPane.LEFT);
+    overviewLabel = new JLabel("Overview");
+    overviewLabel.setPreferredSize(new Dimension(75, 25));
+    overviewTextArea = new JTextArea();
+    driverLabel = new JLabel("Driver");
+    driverLabel.setPreferredSize(new Dimension(75, 25));
+    teamLabel = new JLabel("Teams");
+    teamLabel.setPreferredSize(new Dimension(75, 25));
+    teamTextArea = new JTextArea();
+    trackLabel = new JLabel("Tracks");
+    trackLabel.setPreferredSize(new Dimension(75, 25));
+    trackTextArea = new JTextArea();
+
+    tabbedInnerPane.addTab("", overviewTextArea);
+    tabbedInnerPane.setTabComponentAt(tabbedInnerPane.getTabCount() - 1, overviewLabel);
+    tabbedInnerPane.setTabComponentAt(tabbedInnerPane.getTabCount() - 1, driverLabel);
+    tabbedInnerPane.addTab("", teamTextArea);
+    tabbedInnerPane.setTabComponentAt(tabbedInnerPane.getTabCount() - 1, teamLabel);
+    tabbedInnerPane.addTab("", trackTextArea);
+    tabbedInnerPane.setTabComponentAt(tabbedInnerPane.getTabCount() - 1, trackLabel);
+
+    tabbedOuterPane.addTab(String.valueOf(count), tabbedInnerPane);
+    tabbedOuterPane.setTabComponentAt(tabbedOuterPane.getTabCount() - 1, tabTitelLabel);
+    window.add(tabbedOuterPane);
+}
+public static void main(String[] args) {
         GUI gui=new GUI();
 
 
