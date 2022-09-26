fbabcbb70e4cb77941f2113e4f3b43e41901c104
DarkBlockHD <103499526+DarkBlockHD@users.noreply.github.com>
2022-09-26 11:58:34 +0200

    initial commit

--- src/F1/POINTS.java
+++ src/F1/POINTS.java
@@ -1,5 +1,7 @@
 package F1;
 
+import java.util.Scanner;
+
 import static F1.DRIVER.scanner;
 
 //In Bearbeitung von Kelschi
@@ -16,7 +18,14 @@ public class POINTS {
     int pos10 = 1;
     int posRest = 0;
 
-    public void ranking(){
+    public void punktevergabe(DRIVER[] driver){
+        for(int i = 0; i < driver.length; i++){
+            System.out.println("Fahrer " + driver[i].getFahrerNummer() + " (" + driver[i].getFahrerName() + ") gib deine Platzierung ein:");
+            int position = scanner.nextInt();
+        }
 
+    }
+    //Zitat Kevin: Darf ich mich mit mir Anal verbinden!?
+    public void ranking(){
     }
 }
